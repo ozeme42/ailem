9663c36a6c6bcc2985ea1578db92bed7270cce60
mahmut ozgurdere <ozgurdere@gmail.com>
2025-07-21 04:45:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -42,10 +42,11 @@ const nextConfig = {
   },
   serverExternalPackages: ['framer-motion'],
   experimental: {
-    allowedDevOrigins: [
-      "https://6000-firebase-studio-1752942593865.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev"
-    ]
-  }
+    // allowedDevOrigins has been moved outside of experimental as it's a top-level property
+  },
+  allowedDevOrigins: [
+    "https://6000-firebase-studio-1752942593865.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev"
+  ]
 };
 
 module.exports = nextConfig;
