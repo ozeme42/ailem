b41413ffa157ec6a3cc4cf111f9aad32fd08f395
mahmut ozgurdere <ozgurdere@gmail.com>
2025-07-26 12:10:26 +0000

    masaüstünde üst menülere ana sayfaya dön butonu ekle

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import Link from "next/link";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Button } from "./ui/button";
+import { Home } from "lucide-react";
 
 interface PageHeaderProps {
   title: string;
@@ -13,7 +16,15 @@ export function PageHeader({ title, children }: PageHeaderProps) {
         <SidebarTrigger />
         <h1 className="text-3xl font-bold tracking-tight text-foreground">{title}</h1>
       </div>
-      <div className="flex flex-wrap items-center justify-end gap-2 sm:gap-4 self-end sm:self-center">{children}</div>
+      <div className="flex flex-wrap items-center justify-end gap-2 sm:gap-4 self-end sm:self-center">
+        <Link href="/" className="hidden md:flex">
+            <Button variant="outline">
+                <Home className="mr-2 h-4 w-4" />
+                Ana Sayfa
+            </Button>
+        </Link>
+        {children}
+      </div>
     </header>
   );
 }
