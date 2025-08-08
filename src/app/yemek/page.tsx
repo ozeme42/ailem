ebfe60bf86b21711487e49b4e93b74c9352681ad
mahmut ozgurdere <ozgurdere@gmail.com>
2025-08-08 13:53:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/yemek/page.tsx
+++ src/app/yemek/page.tsx
@@ -4,7 +4,7 @@
 
 import * as React from "react";
 import { PlusCircle, Search, Clock, Soup, Star, ChevronLeft, ChevronRight, XCircle, Wheat, BarChart2, MoreVertical, Edit, Trash2, Calendar as CalendarIcon, Save } from "lucide-react";
-import { format, addDays, startOfWeek, parseISO, subDays, startOfMonth, endOfMonth, endOfDay } from "date-fns";
+import { format, addDays, startOfWeek, parseISO, subDays, startOfMonth, endOfMonth, endOfDay, eachDayOfInterval } from "date-fns";
 import { tr } from "date-fns/locale";
 import { formatDistanceToNow } from 'date-fns';
 
@@ -736,4 +736,5 @@ export default function YemekPlanlamaPage() {
     </>
   );
 }
+
 
