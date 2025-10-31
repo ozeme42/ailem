4f9027d948431a62c07e7d127c470c74717faac3
mahmut ozgurdere <ozgurdere@gmail.com>
2025-10-31 16:22:04 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-'use server';
-// Flows will be imported for their side effects in this file.
-import '@/ai/flows/search-books-flow';
-import '@/ai/flows/generate-shopping-list-flow';
-import '@/ai/flows/migrate-image-flow';
