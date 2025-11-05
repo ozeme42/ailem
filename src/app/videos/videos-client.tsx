6ba59c7882267d1cb8abaa08b889203b6bc17e59
mahmut ozgurdere <ozgurdere@gmail.com>
2025-11-05 13:35:19 +0000

    bunlarıda renklendir (_for element div_)

--- src/app/videos/videos-client.tsx
+++ src/app/videos/videos-client.tsx
@@ -4,7 +4,7 @@
 
 import React, { useState, useMemo, useCallback, useEffect } from 'react';
 import Link from 'next/link';
-import { useForm } from 'react-hook-form';
+import { useForm, FormProvider, useFormContext } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Video, FamilyMember } from '@/lib/data';
