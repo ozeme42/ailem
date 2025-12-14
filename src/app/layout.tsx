956a61a3e0c19a9c2f400fe253e35a567bea057e
mahmut ozgurdere <ozgurdere@gmail.com>
2025-12-14 12:35:59 +0000

    tema rengi de kapalı olsun varsayılan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,7 @@ export default function RootLayout({
       <body className={`${inter.variable} font-sans antialiased`}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="light"
             disableTransitionOnChange
         >
             <AuthProvider>
